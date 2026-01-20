995be7fd03c7fc8010f203335bc5cd67f8673379
Ayush Yadav <0241csds239@niet.co.in>
2026-01-20 23:02:09 +0530

    daily practice

---
+++ program61.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int num[]={10,25,54,1,6,-85,74};
+    int size=7;
+    int smallest = INT_MAX;
+    for (int i=0; i<size; i++){
+        if (num[i] < smallest){
+            smallest = num[i];
+        }
+    }
+    cout << "Smallest number is :" << smallest << endl;
+    return 0;
+}
